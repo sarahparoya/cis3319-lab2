2ba3eed706df764bc2015f60aa64aa995c1be6aa
sarahparoya <sarahparoya@gmail.com>
2021-02-08 23:38:46 -0500

    <3
    finished lol

--- client.py
+++ client.py
@@ -3,24 +3,24 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect(('127.0.0.1', 3000)) #coonect to server
+s.connect(('127.0.0.1', 12345)) #coonect to server
 print(s.recv(1024).decode())
-keystr = "imthekey" #key string
+keystr = "iamakeys" #key string
 key = DesKey(keystr.encode('utf-8')) #create a key
 while True:
-    msg = input("Type message: ")
+    msg = input("type your message: ")
     ct = key.encrypt(msg.encode('utf-8'), padding=True) #encrypt a msg
     print("*"*18)
     print("key is ", keystr)
-    print("Sent plaintext is: %s"%msg)
-    print("Sent ciphertext is: %s"%ct.decode('utf-8', 'ignore'))
+    print("sent plaintext is: %s"%msg)
+    print("sent ciphertext is: %s"%ct.decode('utf-8', 'ignore'))
     print("*"*18)
     s.send(ct) #send ciphertext
     rcv = s.recv(1024) #recieved the msg
     print("*"*18)
-    print("Recieved ciphertext is: %s"%rcv.decode('utf-8', 'ignore'))
+    print("recieved ciphertext is: %s"%rcv.decode('utf-8', 'ignore'))
     pt = key.decrypt(rcv, padding=True).decode() #decrypt the msg
-    print("Recieved plaintext is: %s"%pt)
+    print("recieved plaintext is: %s"%pt)
     print("*"*18)
     if pt == "bye": #if recieved msg is bye then break and close the connection
         break
--- server.py
+++ server.py
@@ -1,31 +1,31 @@
 from des import DesKey
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create a socket
-port = 3000
-s.bind(('', port)) #bind the socket to address
-s.listen(1) #start listening
-keystr = "imthekey" #key string must be multiple of 8
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creating the socket
+port = 12345
+s.bind(('', port)) #binding the socket to address
+s.listen(5) #start listening
+keystr = "iamakeys" #key string must be multiple of 8
 key = DesKey(keystr.encode('utf-8')) #create key
-print("Server is running...")
+print("server is running...")
 conn, addr = s.accept() #accept connection request
-print("Accept new connection from %s"%(str(addr)))
-conn.send(str("Connection established").encode())
+print("accept new connection from %s"%(str(addr)))
+conn.send(str("connection has been established!").encode())
 while True:
     rcv = conn.recv(1024)
     print("*"*18)
-    print("Recieved ciphertext is: %s"%rcv.decode('utf-8', 'ignore'))
+    print("recieved ciphertext is: %s"%rcv.decode('utf-8', 'ignore'))
     pt = key.decrypt(rcv, padding=True).decode() #decrypt the key
-    print("Recieved plaintext is: %s"%pt)
+    print("recieved plaintext is: %s"%pt)
     print("*"*18)
     if pt == "bye": #if recieved msg is bye then break and close the connection
         break
-    msg = input("Type message: ")
+    msg = input("type your message: ")
     ct = key.encrypt(msg.encode('utf-8'), padding=True)
     print("*"*18)
     print("key is ", keystr)
-    print("Sent plaintext is: %s"%msg)
-    print("Sent ciphertext is: %s"%ct.decode('utf-8', 'ignore'))
+    print("sent plaintext is: %s"%msg)
+    print("sent ciphertext is: %s"%ct.decode('utf-8', 'ignore'))
     print("*"*18)
     conn.send(ct) #send ciphertext
 conn.close()
